58893f86a00cb16a71e8c0e463772191ebb34e5d
Genesis Lara <genesislara@pursuit.org>
2023-07-30 16:42:08 -0400

    removes components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App () {
+  const [mode, setMode] = useState();
+
+  function currentScore() {};
+
     return (
+        <>  
       <main>
         <p>React State Lab</p>
       </main>
+      <CurrentScore />
+      <VictoryScreen />
+      </>
     );
 }
 
